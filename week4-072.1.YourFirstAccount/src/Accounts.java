71d1dd4ab8dae4efe91ae752a9cfcec843d7ec41
furkan3 <34316921+furkan3@users.noreply.github.com>
2019-10-11 21:27:32 +0300

    Add files via upload

---
+++ week4-072.1.YourFirstAccount/src/Accounts.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class Accounts {
+
+    public static void main(String[] args) {
+        // Code in Account.Java should not be touched!
+        // write your code here
+        Account furkan= new Account("Furkan Yelen",100.00);
+        furkan.deposit(20);
+        System.out.println(furkan);
+    }
+
+}
